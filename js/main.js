6011ccab4dcf553db368902c56c597c3f5fcae69
pitzbro <asioren@gmail.com>
2016-12-06 16:42:37 +0200

    min js 001

--- js/main.js
+++ js/main.js
@@ -4,53 +4,53 @@ var gItems;
 var galleryCont;
 var gKeyWords = ['fuuny', 'animal', 'yes', 'akward', 'happy', 'sad'];
 
-function init(){
-    galleryCont = document.querySelector('.gallery__container');  
-    renderItems();
+function init() {
+    galleryCont = document.querySelector('.gallery__container');
+    createItems();
 }
-/**fix!!! */
-function createItems(){
-    gItems=[];
-    for (var i = 1; i < 14; i++){
+
+function createItems() {
+    gItems = [];
+    for (var i = 1; i < 14; i++) {
         gItems.push({
-            id  :   i+1,
-            url :   "",
-            keywords : getKeyWords(),
-                })
+            id: i + 1,
+            url: "",
+            keywords: getKeyWords(),
+        })
     }
-
 }
 
-function getKeyWords(){
-    // var keyWords=[];
-
-    // for (var i = 0; i < getRandomInt(2, 4); i++){
-    //     getKeyWords[getRandomInt(0,gKeyWords.length-1)];
-        
-    // }
-}
-
-
-function renderItems (){
+function renderItems() {
     var strHtml;
-    for (var i = 1; i < 14; i++){
-        strHtml += '<img src="assets/memes/'+ i + '.jpg">';
+    for (var i = 1; i < 14; i++) {
+        strHtml += '<img src="assets/memes/' + i + '.jpg">';
     }
-    galleryCont.innerHTML = strHtml;    
+    galleryCont.innerHTML = strHtml;
 }
 
-//  this function filters the meme gallery according to the search/keywords(works every click/type)
-function filterMeme (keyword){
+function getKeyWords() {
+    var keywords = [];
+    for (var i = 0; i < getRandomInt(2, 4); i++) {
+        var keyword = gKeyWords[getRandomInt(0, gKeyWords.length - 1)];
+        // check to see if keyword is present and if not 
+        // push it to keywords
+        while (keywords.indexOf(keyword) === -1) {
+            keywords.push(keyword);
+        }
+    }
+    return keywords;
+}
+
+// this function filters the meme gallery according to the search/keywords
+// (works every click/type)
+function filterMeme(keyword) {
 
 }
 
-function renderKeyWordsList(){
+function renderKeyWordsList() {
 
 }
 
-function editMode(){
-    
+function editMode() {
+    console.log('hello');
 }
-
-
-
