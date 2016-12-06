d96a71f2252473ecd3f42c0115d933cfa88f2eef
Ori Atias <oriati@gmail.com>
2016-12-06 17:30:08 +0200

    commiting now

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,22 @@
 'use strict'
 
-function editMode(memeTemplate){
-     var canvas = document.querySelector('.editor-canvas');
+var canvas;
+var ctx;
+
+
+testCanvas();
+function testCanvas(){
+    var testImg = document.querySelector('#testCan-image');
+    editMode(testImg);
+
 }
+
+function editMode(elImg){
+     canvas = document.querySelector('.editor-canvas');
+     ctx = canvas.getContext('2d');
+     ctx.drawImage(elImg, 0, 0, 568, 360);
+
+}
+
 
 
