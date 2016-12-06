8a5483758512c67e4964fbe67102a4bb254fdbf0
Ori Atias <oriati@gmail.com>
2016-12-06 15:20:42 +0200

    javascript template

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 'use strict';
-console.log('Hello Meme');
+
+var gItems;
+var galleryCont;
+var gKeyWords = ['fuuny', 'animal', 'yes', 'akward', 'happy', 'sad'];
+
+function init(){
+    galleryCont = document.querySelector('.gallery__container');  
+    renderItems();
+}
+
+function createItems(){
+    gItems=[];
+    for (var i = 1; i < 14; i++){
+        gItems.push({
+            id  :   i+1,
+            url :   "",
+            keywords : getKeyWords(),
+                })
+    }
+
+}
+
+function getKeyWords();
+
+
+function renderItems (){
+    var strHtml;
+    for (var i = 1; i < 14; i++){
+        strHtml += '<img src="assets/memes/'+ i + '.jpg">';
+    }
+    galleryCont.innerHTML = strHtml;    
+}
