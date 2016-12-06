cba5d1d16a66e9249b0cae93c59a17707dfc5d92
Ori Atias <oriati@gmail.com>
2016-12-06 19:07:31 +0200

    editor evening

--- js/editor.js
+++ js/editor.js
@@ -7,16 +7,33 @@ var ctx;
 testCanvas();
 function testCanvas(){
     var testImg = document.querySelector('#testCan-image');
-    editMode(testImg);
-
+    createMeme(testImg);
 }
 
-function editMode(elImg){
-     canvas = document.querySelector('.editor-canvas');
-     ctx = canvas.getContext('2d');
-     ctx.drawImage(elImg, 0, 0, 568, 360);
 
+
+function createMeme(elImg) {
+    console.log('creating');
+    canvas = document.querySelector('.editor-canvas');
+    ctx = canvas.getContext('2d');
+    ctx.drawImage(elImg, 25, 25, 600, 600);
+    ctx.font = "60px 'Segoe UI'";
+    var topTxt = document.querySelector('.input-text-top').value;
+    var botTxt = document.querySelector('.input-text-bot').value;
+    ctx.fillText(topTxt, 200, 100);
+    ctx.fillText(botTxt, 200, 550);
 }
+
+// function insertText(ctx) {
+//     ctx.font = "60px 'Segoe UI'";
+//     var topTxt = document.querySelector('.input-text-top').value;
+//     var botTxt = document.querySelector('.input-text-bot').value;
+//     ctx.fillText(topTxt, 200, 100);
+//     ctx.fillText(botTxt, 200, 550);
+// }
+
+
+
 
 
 
